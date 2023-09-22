d514d9ba10844e3c30dd51651cbd6e961d5d11fc
DSORJUELA <orjueladaniel@yahoo.es>
2023-09-22 12:24:43 -0500

    Se comenta codigo para vibración

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 Frm = {
 
   FileChange: function(){
-    // console.log($(this)[0].files[0]);
     let file = $(this)[0].files[0];
     let fr = new FileReader();
     fr.onload = function(){
-      // console.log(fr.result);
       ImageTracer.imageToSVG(fr.result,
         function(svgstr){
           let htmlImage = `<img src="${fr.result}" />`;
@@ -19,14 +17,14 @@ Frm = {
     $('#containerImage').removeClass('d-none');
   },
 
-  Vibration: function(){
-    let seconds = parseInt($('#txtSecondsVibration').val()) * 1000;
-    navigator.vibrate(seconds);
-  },
+  // Vibration: function(){
+  //   let seconds = parseInt($('#txtSecondsVibration').val()) * 1000;
+  //   navigator.vibrate(seconds);
+  // },
 
   Init: function(){
     $('#formFile').change(Frm.FileChange);
-    $('#btnVibration').click(Frm.Vibration)
+    // $('#btnVibration').click(Frm.Vibration);
   }
 };
 
