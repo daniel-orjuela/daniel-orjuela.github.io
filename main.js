7a9210acc7c9f2898c130490faedd251023a499f
DSORJUELA <orjueladaniel@yahoo.es>
2023-09-22 22:36:00 -0500

    Header Datetime

--- main.js
+++ main.js
@@ -22,7 +22,30 @@ Frm = {
   //   navigator.vibrate(seconds);
   // },
 
+  CalculateDateTime: function(){
+    let now = new Date();
+    let date = "";
+    let time = "";
+
+    let month = now.getMonth() + 1;
+    let day = now.getDate();
+    date = day < 10 ? '0' + day : day;
+    date += month < 10 ? '/0' + month : '/' + month;
+    date += '/' + now.getFullYear();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    time = hours < 10 ? "0" + hours : hours;
+    time += minutes < 10 ? ":0" + minutes : ":" + minutes;
+    time += seconds < 10 ? ":0" + seconds : ":" + seconds;
+
+    $('#txtFechaHora').text(date + ' - ' + time);
+  },
+
   Init: function(){
+    Frm.CalculateDateTime();
+    setInterval(Frm.CalculateDateTime, 1000);
     $('#formFile').change(Frm.FileChange);
     // $('#btnVibration').click(Frm.Vibration);
   }
